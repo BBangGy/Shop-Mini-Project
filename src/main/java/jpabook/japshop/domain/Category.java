ffa853ced6f8f3407fd5a1796fa01b577f6c117a
정현규 <chunghyeongyu@jeonghyeongyuui-MacBookPro.local>
2025-01-26 17:19:27 +0900

    made memberRepository and memberService

--- src/main/java/jpabook/japshop/domain/Category.java
+++ src/main/java/jpabook/japshop/domain/Category.java
@@ -9,29 +9,38 @@ import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Getter
 @Setter
 public class Category {
-    @Id @GeneratedValue
-    @Column(name="category_id")
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
     private Long id;
 
     private String name;
 
     @ManyToMany
     @JoinTable(name = "category_item",
-    joinColumns = @JoinColumn(name = "category_id"),
-    inverseJoinColumns = @JoinColumn(name = "item_id"))
-    //중간 테이블을 맵핑 해줘야한다.
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    //중간 테이블을 맵핑 해줘야한다. @JoinTable
     //joincolumn은 중가테이블에 있는 카테고리 id
-    //inversejoincolumns= category_item 이 테이블의 item쪽에 들어가는걸 맵핑
+    //inversejoincolumns은 category_item 테이블에 item쪽에 들어가는걸 맵핑
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
-    @ManyToMany(mappedBy = "parent")
+    @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //연관관계 메서드//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
