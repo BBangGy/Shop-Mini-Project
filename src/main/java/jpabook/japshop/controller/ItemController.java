b1a3cbbb2803f1681e8dcdbef5892edf1de27cb0
정현규 <chunghyeongyu@jeonghyeongyuui-MacBookPro.local>
2025-02-07 15:07:37 +0900

    add a order feature

--- src/main/java/jpabook/japshop/controller/ItemController.java
+++ src/main/java/jpabook/japshop/controller/ItemController.java
@@ -2,13 +2,19 @@ package jpabook.japshop.controller;
 
 import jakarta.validation.Valid;
 import jpabook.japshop.domain.item.Book;
+import jpabook.japshop.domain.item.Item;
 import jpabook.japshop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.ModCheck;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,5 +38,33 @@ public class ItemController {
         itemService.saveItem(book);
         return "redirect:/items";
     }
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItem();
+        model.addAttribute("items",items);
+        return "items/itemList";
+    }
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
+        Book item =(Book) itemService.findOne(itemId);
 
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setIsbn(item.getIsbn());
+        form.setAuthor(item.getAuthor());
+
+        model.addAttribute("form",form);
+        return "items/updateItemForm";
+    }
+////        @ModelAttribute("form") → 폼 데이터를 BookForm 객체에 바인딩함.
+////        "form" → 뷰에서 접근할 때 사용할 이름. -> th:object="${form}"
+////        BookForm form → 사용자가 입력한 데이터가 들어 있는 객체.
+    @PostMapping(value = "/items/{itemId}/edit")
+    public String updateItem(@PathVariable("itemId")Long itemId, @ModelAttribute("form") BookForm form) {
+        itemService.updateItem(itemId,form.getName(), form.getPrice(), form.getStockQuantity());
+        return "redirect:/items";
+    }
 }
--- src/main/java/jpabook/japshop/service/ItemService.java
+++ src/main/java/jpabook/japshop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.japshop.service;
 
+import jpabook.japshop.domain.item.Book;
 import jpabook.japshop.domain.item.Item;
 import jpabook.japshop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     public void saveItem(Item item){
         itemRepository.save(item);
     }
+
+    //변경 감지 기능 사용->엔티티를 변경할때 항상 이걸 사용.
+    //업데이트 할 부분만 결정해서 적용한다. merge는 절대 쓰지마.
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+    }
+
     public List<Item> findItem(){
         return itemRepository.findAll();
     }
